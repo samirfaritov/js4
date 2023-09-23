a922759b3110ad1ab6f3f4a5b170d520c08aaa91
Samir4ik <145694562+samirfaritov@users.noreply.github.com>
2023-09-23 15:07:38 +0500

    js4

---
+++ main.js
@@ -0,0 +1,26 @@
+let str = "aaa@bbb@ccc"
+let a = str.replace("@", "!").replace("@", "!")
+
+console.log(a);
+
+
+let str1 = "HELLO WORLD"
+let b = str1.toLowerCase()
+
+console.log(b);
+
+let html = "Hello, it is HTML"
+let js = html.replace("HTML", "not").concat(" JS")
+
+console.log(js);
+
+let u = "alex"
+let q = u.slice(0, 4)
+console.log(u);
+console.log(q);
+
+let rnd = Math.random() * 100
+let x = parseInt(rnd)
+console.log(x);
+
+let s = "Lex Luter has a big suit"
